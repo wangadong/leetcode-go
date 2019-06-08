5e7daed7bda23ba9ae4ba7d5d3516afc1ea91f81
Xindong Wang <tonywangadong@gmail.com>
2019-06-07 21:17:50 -0400

    Create remove_element_27.go

---
+++ algorithms/remove_element_27.go
@@ -0,0 +1,11 @@
+func removeElement(nums []int, val int) int {
+    nextIndex := 0
+    for _, num := range nums {
+        if num != val {
+            nums[nextIndex] = num
+            nextIndex++
+            continue
+        }
+    }
+    return nextIndex
+}
